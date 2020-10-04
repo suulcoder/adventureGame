5fa001838a222650af733bdec88b85d463623f99
Saúl Contreras <suulcoder@gmail.com>
2020-10-03 23:39:19 -0600

    Add files via upload

---
+++ Scripts/message.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class message : MonoBehaviour
+{
+    public GameObject particle1;
+    public GameObject particle2;
+    public GameObject earth;
+    public GameObject mars;
+    public bool activate = false;
+
+    public void generate()
+    {
+        activate = true;
+    }
+
+    public void changeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private bool evaluateDistance(Vector3 initial, Vector3 end)
+    {
+        Vector3 diference = end - initial;
+        return diference.x > 0 || diference.y > 0 || diference.z > 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Vector3 myPosition = transform.position;
+        particle1.transform.position = myPosition;
+        particle2.transform.position = myPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activate)
+        {
+            float myScale = 0.001f;
+            Vector3 toearth = (earth.transform.position - transform.position);
+            Vector3 toMars = (mars.transform.position - transform.position);
+
+            if (evaluateDistance(particle1.transform.position, earth.transform.position))
+            {
+                particle1.transform.position += new Vector3(
+                    toearth.x * myScale,
+                    toearth.y * myScale,
+                    toearth.z * myScale
+                );
+            }
+
+            if (evaluateDistance(mars.transform.position, particle2.transform.position))
+            {
+                particle2.transform.position += new Vector3(
+                    toMars.x * myScale,
+                    toMars.y * myScale,
+                    toMars.z * myScale
+                );
+            }
+        }
+    }
+}
---
+++ Scripts/signal.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class signal : MonoBehaviour
+{
+
+    public GameObject satellite;
+    public GameObject particle;
+    private bool onCircuit = false;
+    private bool onMars = false;
+    private int state = 0;
+    public int counter = 0;
+    public GameObject reader;
+    public GameObject writer;
+    public GameObject toSpace;
+    public Light voltimeter;
+    public Slider energy;
+
+    public void write(bool isZero = false)
+    {
+        writer.transform.position = isZero ? new Vector3(570, 67, 0) : new Vector3(570, -250, 0);
+    }
+
+    public void changeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        toSpace.active = false;
+        if (onMars)
+        {
+            energy.value -= 0.0003f;
+            if (energy.value <= 0)
+            {
+                toSpace.active = true;
+                Destroy(particle);
+            }
+            else
+            {
+                toSpace.active = false;
+            }
+        }
+
+        voltimeter.color = onMars ? Color.green : Color.red;
+        float scale = 0.005f;
+        Vector3 direction = satellite.transform.position - transform.position;
+        direction.x *= scale;
+        direction.y *= scale;
+        direction.z *= scale;
+
+        if (particle)
+        {
+            Vector3 distance = particle.transform.position - transform.position;
+            if (distance.x > 0 & distance.y > 0 & distance.z > 0 & !onMars)
+            {
+                particle.transform.position -= direction;
+            }
+            else
+            {
+                onMars = true;
+            }
+            if (onMars & !onCircuit)
+            {
+                particle.transform.position = new Vector3(0, 1.6f, 0);
+                onCircuit = true;
+            }
+            else if (onCircuit)
+            {
+                counter++;
+                if (counter > 200)
+                {
+                    counter = 0;
+                }
+
+                if (counter < 100)
+                {
+                    reader.transform.position = new Vector3(365, 67, 0);
+                }
+                else
+                {
+                    reader.transform.position = new Vector3(-115, -250, 0);
+                }
+
+                scale = 0.013f;
+                switch (state)
+                {
+                    case 0:
+                        direction = new Vector3(5, 1.6f, 0) - new Vector3(0, 1.6f, 0);
+                        direction.x *= scale;
+                        direction.y *= scale;
+                        direction.z *= scale;
+                        particle.transform.position += direction;
+                        distance = new Vector3(5, 1.6f, 0) - particle.transform.position;
+                        if (distance.x < 0)
+                        {
+                            state = 1;
+                        }
+                        break;
+                    case 1:
+                        direction = new Vector3(0, 1.6f, 8.66025403784f) - new Vector3(5, 1.6f, 0);
+                        direction.x *= scale;
+                        direction.y *= scale;
+                        direction.z *= scale;
+                        particle.transform.position += direction;
+                        distance = new Vector3(0, 1.6f, 8.66025403784f) - particle.transform.position;
+                        if (distance.x > 0 & distance.z < 0)
+                        {
+                            state = 2;
+                        }
+                        break;
+                    case 2:
+                        direction = new Vector3(-5, 1.6f, 0) - new Vector3(0, 1.6f, 8.66025403784f);
+                        direction.x *= scale;
+                        direction.y *= scale;
+                        direction.z *= scale;
+                        particle.transform.position += direction;
+                        distance = new Vector3(-5, 1.6f, 0) - particle.transform.position;
+                        if (distance.x > 0 & distance.z > 0)
+                        {
+                            state = 3;
+                        }
+                        break;
+                    case 3:
+                        direction = new Vector3(0, 1.6f, 0) - new Vector3(-5, 1.6f, 0);
+                        direction.x *= scale;
+                        direction.y *= scale;
+                        direction.z *= scale;
+                        particle.transform.position += direction;
+                        distance = new Vector3(0, 1.6f, 0) - particle.transform.position;
+                        if (distance.x < 0)
+                        {
+                            state = 0;
+                        }
+                        break;
+                }
+            }
+        }
+        
+        
+
+
+    }
+}
